f62cebc0ed5f6d0f61d71f2b239a5147bf210206
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2022-06-03 12:18:15 +0300

    Added MathType plugin

---
+++ scripts/main.js
@@ -0,0 +1,81 @@
+(function(window, undefined){
+	window.Asc.plugin.init = function(sMathML)
+	{
+		if (sMathML !== "")
+			editor.setMathML(sMathML);
+	};
+
+	function render_formula(sMathML){
+		var oReq = new XMLHttpRequest();
+		oReq.open("POST", 'https://www.wiris.net/demo/editor/render.png', true);
+		oReq.responseType = 'blob';
+		oReq.setRequestHeader("Content-Type", 'application/x-www-form-urlencoded');
+		
+		oReq.onload = function(e) {
+			var reader = new FileReader();
+			reader.readAsDataURL(this.response); 
+			reader.onloadend = function() {
+				var base64data = reader.result;
+				var oImg = new Image(); 
+				oImg.onload = function() {
+					var oInfo = window.Asc.plugin.info;
+
+					var sMethod = (oInfo.objectId === undefined) ? "AddOleObject" : "EditOleObject";
+					
+					var _param = {
+						guid:      oInfo.guid,
+						widthPix:  (oInfo.mmToPx * oImg.width) >> 0,
+						heightPix: (oInfo.mmToPx * oImg.height) >> 0,
+						width:     (oImg.width / oInfo.mmToPx) / 6,
+						height:    (oImg.height / oInfo.mmToPx) / 6,
+						imgSrc:    base64data,
+						data:      sMathML,
+						objectId:  oInfo.objectId,
+						resize:    oInfo.resize
+					};
+
+					window.Asc.plugin.executeMethod(sMethod, [_param]);
+				};
+				
+				oImg.src = base64data;
+			}
+		}
+		oReq.send(jQuery.param({
+			mml: sMathML,
+			autozoom: true,
+			centerbaseline: false
+		}));
+	}
+
+	function paste_formula(){
+		var sMathML = editor.getMathML();
+		var oPng = render_formula(sMathML);
+	}
+
+	$(document).ready(function () {
+		editor = com.wiris.jsEditor.JsEditor.newInstance({'language': 'en'});
+        editor.insertInto(document.getElementById('editorContainer'));
+		if (window.location.href.search("type=chemistry") !== -1) {
+			editor.setParams({
+				toolbar: "chemistry"
+			});
+		}
+	})
+
+	window.Asc.plugin.button = function(id)
+	{
+		if (id === 0)
+			paste_formula();
+		else
+			this.executeCommand("close", "");
+	};
+
+	window.Asc.plugin.onExternalMouseUp = function()
+	{
+		var evt = document.createEvent("MouseEvents");
+		evt.initMouseEvent("mouseup", true, true, window, 1, 0, 0, 0, 0,
+			false, false, false, false, 0, null);
+
+		document.dispatchEvent(evt);
+	};
+})(window, undefined);
