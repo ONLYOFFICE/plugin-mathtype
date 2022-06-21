5cccd4e068f0b2276828fe866a2d2d5374cb3f75
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2022-06-21 17:51:48 +0300

    Added calc baseline

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,51 @@
 (function(window, undefined){
+	var langs_map = {
+		"ca-CA": "ca",
+		"zh-ZH": "zh",
+		"cs-CS": "cs",
+		"da-DA": "da",
+		"nl-NL": "nl",
+		"en-EN": "en",
+		"fi-FI": "fi",
+		"fr-FR": "fr",
+		"de-DE": "de",
+		"el-EL": "el",
+		"hu-HU": "hu",
+		"id-ID": "id",
+		"it-IT": "it",
+		"ja-JA": "ja",
+		"ko-KO": "ko",
+		"nb-NB": "nb",
+		"pl-PL": "pl",
+		"pt-PT": "pt",
+		"pt-BR": "pt_br",
+		"ro-RO": "ro",
+		"ru-RU": "ru",
+		"es-ES": "es",
+		"sv-SV": "sv",
+		"tr-TR": "tr"
+	}
+
+	var isLoaded = false;
+	var plugin_lang = "en";
+
 	window.Asc.plugin.init = function(sMathML)
 	{
+		if (langs_map[window.Asc.plugin.info.lang] != undefined)
+			plugin_lang = langs_map[window.Asc.plugin.info.lang];
+
+		if (!isLoaded) {
+			
+			editor = com.wiris.jsEditor.JsEditor.newInstance({'language': plugin_lang});
+        	editor.insertInto(document.getElementById('editorContainer'));
+
+			if (window.location.href.search("type=chemistry") !== -1) {
+				editor.setParams({
+					toolbar: "chemistry"
+				});
+			}
+		}
+
 		window.Asc.plugin.resizeWindow(600, 310, 600, 395, 0, 0);
 		if (sMathML !== "")
 			editor.setMathML(sMathML);
@@ -22,9 +67,13 @@
 					var oInfo = window.Asc.plugin.info;
 
 					var sMethod = (oInfo.objectId === undefined) ? "AddOleObject" : "EditOleObject";
-					
+					var nFormulaSourceHeight = editor.editorModel.formulaModel.getHeight();
+					var nBaseLineFromTop = editor.editorModel.getFormulaBaseline();
+					var nRelBaseLine = 1 - nBaseLineFromTop / nFormulaSourceHeight;
+
 					var oParams = {
 						guid:      oInfo.guid,
+						position:  -((oImg.height * nRelBaseLine) / (oInfo.mmToPx * 6)),
 						widthPix:  (oInfo.mmToPx * oImg.width) >> 0,
 						heightPix: (oInfo.mmToPx * oImg.height) >> 0,
 						width:     (oImg.width / oInfo.mmToPx) / 6,
@@ -57,16 +106,6 @@
 		var sMathML = editor.getMathML();
 		render_formula(sMathML, sImgFormat);
 	}
-
-	$(document).ready(function () {
-		editor = com.wiris.jsEditor.JsEditor.newInstance({'language': 'en'});
-        editor.insertInto(document.getElementById('editorContainer'));
-		if (window.location.href.search("type=chemistry") !== -1) {
-			editor.setParams({
-				toolbar: "chemistry"
-			});
-		}
-	});
 
 	window.Asc.plugin.button = function(id)
 	{
